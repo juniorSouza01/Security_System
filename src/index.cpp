567ac7edec80ebb24996e279f5d6d6ad51f7cf24
Gelado047 <juniorthesouza017@gmail.com>
2023-08-22 13:21:45 -0300

    setp1

---
+++ src/index.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    int a, i = 0;
+    string text, old, password, password2, pass, name, password0, age, user, word, word1;
+    string creds[2], cp[2];
+
+    cout<<"       Security System"<<endl;
+
+    cout<<" __________________________________"<<endl<<endl;
+    cout<<"|         1°  Register             |"<<endl;
+    cout<<"|         2°  Login                |"<<endl;
+    cout<<"|         3°  Change password      |"<<endl;
+    cout<<"|_________4°  End Program__________|"<<endl;
+}
